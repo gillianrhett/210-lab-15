36ab207d4af2f39847169c9a3eca859ba7b3ed77
gillianrhett <gillianrhett@gmail.com>
2026-03-01 17:39:00 -0800

    working on dealing with strings from file

--- 210-lab-15.cpp
+++ 210-lab-15.cpp
@@ -16,7 +16,7 @@ class Movie {
     public:
         // setters and getters
         void setYear(int year) { // TODO data validation
-
+            this->year = year;
         }
         void setWriter(string writer) { this->writer = writer; } // inline function to set the screenwriter's name
         void setTitle(string title) { this->title = title; } // inline function to set the movie title
@@ -47,7 +47,36 @@ int main() {
     }
 
     vector<Movie> movies; // empty vector to store movies from the file
+
+    // read each line from the file and store the values in a Movie object
+    string tempInput; // for getting each line from the file
+    int tempInt; // for validating input
+    bool validInput = false;
+    while(!moviesFile.eof()) {
+        Movie tempMovie; // this will store the values then a copy will be added to the vector
+        
+        moviesFile >> tempInput; // read the first line of this movie, the title
+        tempMovie.setTitle(tempInput);
+        
+        moviesFile >> tempInput; // read the second line of this movie, the year
+        try {
+            tempInt = stoi(tempInput);
+        }
+        catch(const exception& e) {
+            moviesFile.clear(); // skip this line
+            tempInt = -1; // rather than ending the program, put this in to indicate there wasn't a valid year            
+        }
+        tempMovie.setYear(tempInt);
+
+        moviesFile >> tempInput; // read the third line of this movie, the writer's name
+        tempMovie.setWriter(tempInput);
+
+        movies.push_back(tempMovie); // store a copy of this movie in the vector
+    }
     
+    // show the info for the movies stored in the vector
+    for (Movie eachMovie : movies)
+        eachMovie.print();    
 
     return 0;
 }
